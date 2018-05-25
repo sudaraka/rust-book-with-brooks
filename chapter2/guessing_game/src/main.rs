92798d7838c3697e823094fb7b443fb08b60846b
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2018-05-26 02:03:50 +0530

    Add chapter2 book example

---
+++ chapter2/guessing_game/src/main.rs
@@ -0,0 +1,44 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // shadow previous declaration of `guess` (as String) by converting it
+        // to a u32
+        //
+        // IMPORTANT: `trim()` here is very important to remove `\n` in the user
+        // input, Otherwise the `parse()` will always fail.
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
