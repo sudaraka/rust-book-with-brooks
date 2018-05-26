a9a3fc6696c6365eaa2187f60aba8a601ea60343
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2018-05-26 18:32:49 +0530

    Move guessing algorithm to a function

--- chapter2/reverse_guessing_game/src/main.rs
+++ chapter2/reverse_guessing_game/src/main.rs
@@ -1,17 +1,7 @@
 use std::cmp::Ordering;
 use std::io::stdin;
 
-fn main() {
-    println!("Welcome to the reverse guessing game!");
-    println!("Please guess a number between 1 and 100");
-
-    let mut secret_number = String::new();
-
-    stdin()
-        .read_line(&mut secret_number)
-        .expect("Failed to read user input");
-
-    let secret_number: i32 = secret_number.trim().parse().expect("Not a valid number");
+fn guess_number(secret_number: i32) -> i32 {
     let mut guess = 4;
     let diff = guess / 2;
 
@@ -25,11 +15,22 @@ fn main() {
             Ordering::Less => {
                 guess -= diff / 2;
             }
-            Ordering::Equal => {
-                println!("Your secret number id {}", guess);
-
-                break;
-            }
+            Ordering::Equal => return guess,
         }
     }
+}
+
+fn main() {
+    println!("Welcome to the reverse guessing game!");
+    println!("Please guess a number between 1 and 100");
+
+    let mut secret_number = String::new();
+
+    stdin()
+        .read_line(&mut secret_number)
+        .expect("Failed to read user input");
+
+    let secret_number: i32 = secret_number.trim().parse().expect("Not a valid number");
+
+    println!("Your secret number id {}", guess_number(secret_number));
 }
